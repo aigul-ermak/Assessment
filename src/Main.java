086617b7c1ea1c4b661073de119cd8a7d658a715
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-05-19 08:55:33 -0300

    Added changes (added switch)

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,42 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        CeilingFun ceilingFun = new CeilingFun();
+        LocalDate today = LocalDate.now();
+        //LocalDate today = LocalDate.of(2024, 12, 25);
 
-        System.out.println("Ceiling Fun Control");
+        CeilingFun ceilingFun = new CeilingFun(today);
+
+        System.out.println("Ceiling Fun Control system");
         System.out.println("Press 'y' to increase speed, 'r' to reverse direction, 'e' to exit.");
 
+        label:
         while (true) {
-            System.out.println("Current Speed: " + ceilingFun.getSpeed() + ", Direction: " + (ceilingFun.isForward() ? "Forward" : "Reverse"));
+            System.out.println("Current Speed: " + ceilingFun.getSpeed());
+            System.out.println("Direction: " + (ceilingFun.isForward() ? "Forward" : "Reverse"));
+
 
             String inputUser = sc.nextLine().toLowerCase();
 
-
-            if (inputUser.equals("y")) {
-                ceilingFun.increaseSpeedCord();
-                System.out.println("Speed increased to: " + ceilingFun.getSpeed());
-            } else if (inputUser.equals("r")) {
-                ceilingFun.toggleDirection();
-                System.out.println("Direction changed to: " + (ceilingFun.isForward() ? "Forward" : "Reverse"));
-                ceilingFun.increaseSpeedCord();
-            }
-            else if (inputUser.equals("e")) {
-                System.out.println("Exit");
-                break;
-            }else {
-                System.out.println("Invalid input. Please enter 'y', 'n'");
-                break;
+            switch (inputUser) {
+                case "y":
+                    ceilingFun.increaseSpeedCord();
+                    System.out.println("Speed increased to: " + ceilingFun.getSpeed());
+                    break;
+                case "r":
+                    ceilingFun.toggleDirection();
+                    System.out.println("Direction changed to: " + (ceilingFun.isForward() ? "Forward" : "Reverse"));
+                    ceilingFun.increaseSpeedCord();
+                    break;
+                case "e":
+                    System.out.println("Exit from Ceiling Fun Control system");
+                    break label;
+                default:
+                    System.out.println("Invalid input. Please enter 'y', 'n' or 'e'");
+                    break;
             }
         }
 
